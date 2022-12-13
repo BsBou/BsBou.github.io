4a334092bee864e25da3d0563a37c9d2a6cf4fbf
BsBou <borhan.boulandier@gmail.com>
2022-12-13 20:30:37 +0000

    created react app and made banner

---
+++ src/components/Banner/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner-text">
+        <h1>
+          Hello, I'm Borhan Boulandier
+          <br />A full-stack web developer
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/components/Layout/index.js
@@ -0,0 +1,11 @@
+import Banner from '../Banner'
+import './index.css'
+const Layout = () => {
+  return (
+    <div className="page">
+      <Banner />
+    </div>
+  )
+}
+
+export default Layout
