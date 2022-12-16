f023e0d76d147e5e68806017404d6fce7748535c
BsBou <borhan.boulandier@gmail.com>
2022-12-16 13:15:56 +0000

    config for deployment to github pages

--- src/components/Stack/index.js
+++ src/components/Stack/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { Autoplay, Navigation, Pagination, freeMode } from 'swiper'
+// import React, { useEffect, useState } from 'react'
+import { Autoplay, Navigation, Pagination } from 'swiper'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
