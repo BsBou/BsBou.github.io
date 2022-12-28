3321f232abec470a8226f68f519809c0191b2451
BsBou <borhan.boulandier@gmail.com>
2022-12-28 19:43:38 +0000

    responsive styling projects section

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -19,24 +19,28 @@ const projectsArray = [
   },
 ]
 const projectCards = projectsArray.map((project) => (
-  <div className="flex flex-col items-center">
+  <div className="flex flex-col items-center mb-16">
     <div>
-      <img src={project.img} alt="project demo"></img>
-    </div>
-    <div className="w-4/5 p-4 border bg-gray-50 relative bottom-5 left-5">
-      <h1 className="text-center text-xl">{project.name}</h1>
-      <p>{project.desc}</p>
-      <div className="flex justify-end">
-        <span className="mx-4">
-          <a href={project.url} target="_blank" rel="noreferrer">
-            Live demo
-          </a>
-        </span>
-        <span>
-          <a href={project.github} target="_blank" rel="noreferrer">
-            Github
-          </a>
-        </span>
+      <img
+        className="w-full md:w-3/5 relative hover:z-10"
+        src={project.img}
+        alt="project demo"
+      ></img>
+      <div className="w-4/5 p-4 border bg-gray-50 relative bottom-5 left-5 md:bottom-48 md:w-3/5 md:left-1/3">
+        <h1 className="text-center text-xl">{project.name}</h1>
+        <p>{project.desc}</p>
+        <div className="flex justify-end">
+          <span className="mx-4">
+            <a href={project.url} target="_blank" rel="noreferrer">
+              Live demo
+            </a>
+          </span>
+          <span>
+            <a href={project.github} target="_blank" rel="noreferrer">
+              Github
+            </a>
+          </span>
+        </div>
       </div>
     </div>
   </div>
