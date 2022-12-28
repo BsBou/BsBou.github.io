496930c50314497751c58c634f44bdffb19750db
BsBou <borhan.boulandier@gmail.com>
2022-12-28 17:27:43 +0000

    basic styling project cards, added second project

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,21 +1,51 @@
 import './index.css'
-// import WhereInTheWorldMobile from '../../assets/images/where-in-the-world-mobile.gif'
-
+import TraceGif from '../../assets/images/trace.gif'
+import WhippetGood from '../../assets/images/whippet-good-screen.png'
 const projectsArray = [
+  // Ensure web project images are at 16:9 ratio
   {
-    name: 'Where in the world?',
-    // mobileImg: WhereInTheWorldMobile,
+    name: 'Trace',
+    img: TraceGif,
     url: 'https://bsbou.github.io/where-in-the-world-/',
+    github: 'https://github.com/BsBou/trace',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing el  it, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    name: 'Whippet Good',
+    img: WhippetGood,
+    url: 'https://bsbou.github.io/where-in-the-world-/',
+    github: 'https://github.com/BsBou/whippet-good',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing el  it, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
 ]
+const projectCards = projectsArray.map((project) => (
+  <div className="flex flex-col items-center">
+    <div>
+      <img src={project.img} alt="project demo"></img>
+    </div>
+    <div className="w-4/5 p-4 border bg-gray-50 relative bottom-5 left-5">
+      <h1 className="text-center text-xl">{project.name}</h1>
+      <p>{project.desc}</p>
+      <div className="flex justify-end">
+        <span className="mx-4">
+          <a href={project.url} target="_blank" rel="noreferrer">
+            Live demo
+          </a>
+        </span>
+        <span>
+          <a href={project.github} target="_blank" rel="noreferrer">
+            Github
+          </a>
+        </span>
+      </div>
+    </div>
+  </div>
+))
 const Projects = () => {
   return (
     <section id="projects">
-      <h1 className="text-5xl p-4 h-screen">Projects</h1>
-      <div>
-        <h1>{projectsArray[0].name}</h1>
-        <img src={projectsArray[0].mobileImg} alt="project demo"></img>
-      </div>
+      <h1 className="text-5xl p-4 ">Projects</h1>
+      {projectCards}
     </section>
   )
 }
