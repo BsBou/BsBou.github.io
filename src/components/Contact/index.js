a4834a3e7e4a12220da8eecee3d69bf8f805ccd2
BsBou <borhan.boulandier@gmail.com>
2022-12-30 20:23:02 +0000

    added captcha to contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,7 +6,7 @@ const Contact = () => {
   const YOUR_SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE
   const YOUR_TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE
   const YOUR_PUBLIC_KEY = process.env.REACT_APP_EMAILJS_PUBLIC
-
+  const RECAPTCHA_SITE_KEY = process.env.REACT_APP_RECAPTCHA_SITE_KEY
   const form = useRef()
 
   const sendEmail = (e) => {
@@ -30,15 +30,17 @@ const Contact = () => {
   }
 
   return (
-    <section id="contact">
+    <section id="contact" className="pb-20">
       <h1 className="text-5xl p-4 ">Contact</h1>
       <form ref={form} onSubmit={sendEmail} className="flex flex-col">
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input type="text" name="user_name" className="border" />
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input type="email" name="user_email" className="border" />
         <label>Message</label>
-        <textarea name="message" />
+        <textarea name="message" className="border mb-4" />
+        <div class="g-recaptcha" data-sitekey={RECAPTCHA_SITE_KEY}></div>
+        <br />
         <input type="submit" value="Send" />
       </form>
     </section>
