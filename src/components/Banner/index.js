a518340a72fabdbce33a1fe817e075f3263f2d3f
BsBou <borhan.boulandier@gmail.com>
2023-01-15 11:06:23 +0000

    added framer motion, animated see more button

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,10 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleArrowDown } from '@fortawesome/free-solid-svg-icons'
 import './index.css'
+import { motion } from 'framer-motion'
 
 const Banner = () => {
+  const handleClickScroll = () => {
+    const element = document.getElementById('about')
+
+    console.log(element)
+    if (element) {
+      // 👇 Will scroll smoothly to the top of the next section
+      element.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
   return (
-    <div className="banner flex h-screen justify-center items-center">
+    <div
+      id="banner"
+      className="banner flex h-screen justify-center items-center"
+    >
       <div className="banner-text   flex flex-col justify-between h-full">
         {/* mobile:h-1/5 h-1/3 */}
         <div className="h-4/5 flex flex-col justify-center">
@@ -18,9 +32,15 @@ const Banner = () => {
         </div>
         <div className=" flex flex-col h-1/5">
           <h2 className="text-3xl text-center pb-4">See more</h2>
-          <a href="#about" className="text-center">
+          <motion.button
+            animate={{ rotate: [180, 0] }}
+            transition={{ duration: 0.5 }}
+            href="#about"
+            className="text-center"
+            onClick={handleClickScroll}
+          >
             <FontAwesomeIcon icon={faCircleArrowDown} size="3x" />
-          </a>
+          </motion.button>
         </div>
       </div>
     </div>
